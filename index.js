7f5b40a34ac9a80165726d60a19082b8c1dd765e
Vince Banzon <vincebanzon@gmail.com>
2024-08-23 21:17:07 +0800

    Can now sign in

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // for CORS
 const cors = require('cors');
@@ -23,7 +25,7 @@ let Score;
 // Connect to database
 mongoose.connect(DB_CONFIG.CONN_STR, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
   // Create models
-  User = mongoose.model('User', { name: String });
+  User = mongoose.model('User', { username: String, password: String });
   Puzzle = mongoose.model('Puzzle', { name: String });
   Score = mongoose.model('Score', { name: String });
 
@@ -39,9 +41,101 @@ mongoose.connect(DB_CONFIG.CONN_STR, { useNewUrlParser: true, useUnifiedTopology
 mongoose.set('useFindAndModify', false);
 
 
+
+
+
+// MIDDLEWARE
+
+const verifyToken = (req, res, next) => {
+  next();
+}
+
+
+async function checkDuplicateUsername (req, res, next) {
+  User.findOne({username: req.body.username}).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: "Error Auth 1: "+err });
+      return;
+    }
+
+    if (user) {
+      res.status(400).send({ message: 'Failed! Username is already in use!' });
+      return;
+    }
+
+    next();
+  });
+};
+
+async function signup (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, 8),
+  });
+
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: "Error Sign up: "+err });
+      return;
+    }
+
+    res.send({ message: 'User registered successfully!' });
+  });
+};
+
+async function signin (req, res) {
+  User.findOne({
+    username: req.body.username
+  })
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({ message: "Error signing in : "+err });
+        return;
+      }
+
+      if (!user) {
+        return res.status(404).send({ message: 'User Not found.' });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: 'Invalid Password!'
+        });
+      }
+
+      var token = jwt.sign({ id: user.id }, DB_CONFIG.AUTH_SECRET, {
+        expiresIn: 86400 // 24 hours
+      });
+
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        accessToken: token
+      });
+    });
+}
+
+
+
+
+
+const router = express.Router();
 const v1 = '/api/v1';
-// app.use('/', indexRouter);
-// app.use(v1+'/etc', etcRouter);
+
+// Root test route
+app.get('/', (req, res) => {
+  res.send({message: 'API works'})
+})
+
+app.post(v1+'/user/signup', checkDuplicateUsername, signup);
+app.post(v1+'/user/signin', signin);
+
 
 // const user = new User({ name: 'Zildjian' });
 // const puzzle = new Puzzle({ name: 'Zildjian' });
@@ -51,5 +145,5 @@ const v1 = '/api/v1';
 // puzzle.save().then(() => console.log('meow'));
 // score.save().then(() => console.log('meow'));
 
-const PORT= 3000;
+const PORT= 4200;
 app.listen(PORT, () => console.log(`Commissary web service app listening on port ${PORT}`));
